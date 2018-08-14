cf0d71defa732bd7d2cc94206a45594730c44a94
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-14 09:41:42 -0600

    Created all CSS LINKAGE RESET filled Firebase Code inserted

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    // LOAD FIREBASE:
+  var config = {
+    apiKey: "AIzaSyD3IJWGZVt6nfGeMNNtibr4yYElQmVl8ac",
+    authDomain: "train-scheduler-7002e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7002e.firebaseio.com",
+    projectId: "train-scheduler-7002e",
+    storageBucket: "",
+    messagingSenderId: "234382501826"
+  };
+  firebase.initializeApp(config);
+
+}); // DOCUMENT READY FUNCTION END.
