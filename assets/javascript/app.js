0a749dec6e20e6e1ed9fbbc0b84aa747582417fd
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-14 09:52:27 -0600

    Added Structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,9 +6,17 @@ $(document).ready(function() {
     authDomain: "train-scheduler-7002e.firebaseapp.com",
     databaseURL: "https://train-scheduler-7002e.firebaseio.com",
     projectId: "train-scheduler-7002e",
-    storageBucket: "",
+    storageBucket: "train-scheduler-7002e.appspot.com",
     messagingSenderId: "234382501826"
-  };
+  }; // CONFIG END.
   firebase.initializeApp(config);
+
+// CREATE DATABASE REFERENCE FOR FIREBASE
+
+var database = firebase.database();
+
+// CREATE AN ON CLICK FUNCTION: 
+
+
 
 }); // DOCUMENT READY FUNCTION END.
