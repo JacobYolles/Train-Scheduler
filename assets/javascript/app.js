4e9d975c199f6d4a7a3a6959a6c1d12ef66baeae
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-15 12:54:17 -0600

    Added base level functionality code off still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,50 @@ $(document).ready(function() {
 var database = firebase.database();
 
 // CREATE AN ON CLICK FUNCTION: 
+$("#add-train-btn").on("click", function(event) {
+  // PREVENTS THE APP FROM SENDING
+  event.preventDefault();
+  //GRAB THE FORM INPUT ON THE TRAIN
+  // CHANGES THE HTML
+  var trainName = $("#train-name-input").val().trim();
+  var trainDestination = $("#destination-input").val().trim();
+  var trainStart = $("#first-train-input").val().trim(); // CHANGE TO TRAIN FIRST TIME
+  var trainRate = $("#frequency-input").val().trim(); // CHANGE TO TRAIN FREQUENCY
+  // CREATES THE TEMPORARY VARIABLE FOR HOLDING THE DATA OF THE TRAIN, THIS ALLOWS IT TO BECOME MULTIPLE SUBMISSIONS.
+  var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    start: trainStart,
+    rate: trainRate
+  }; // END FOR THE NEW TRAIN VARIABLE
+  //PUSH THE DATA THE IS THERE TO THE NEW TRAIN VARIABLE.
+  // DATABASE .REF .push
+  database.ref().push(newTrain);
 
-// DATABASE .REF .push
+  // CLEARS ALL OF THE DATA AFTER THE FORM IS SUBMITTED.
+  $("#train-name-input").val(""); // TRAIN NAME
+  $("#destination-input").val("");  // TRAIN DESTINATION
+  $("#first-train-input-input").val("");  //FIRST TRAIN TIME
+  $("#frequency-input").val("");  // TRAIN FREQUENCY
+}); // END OF THE ON CLICK EVENT.
+//
+database.ref().on('child_added', getData, error);
+
+function getData(snapshot) {
+  var data = snapshot.val();
+
+  var startTime = moment(data.start, 'HH:mm');
+  console.log(startTime);
+  var difference = moment().diff(moment(startTime), 'minutes');
+  console.log(difference);
+  var remainder = difference % parseInt(data.rate);
+  console.log(remainder);
+  var minutesAway = parseInt(data.rate) - remainder;
+  console.log(minutesAway);
+  var nextArrival = moment().add(minutesAway, 'minutes');
+  nextArrival = moment(nextArrival).format('HH:mm');
+  console.log(nextArrival);
+
+} // END OF THE FUNCTION GET DATA(SNAPSHOT) FUNCTION.
 
 }); // DOCUMENT READY FUNCTION END.
