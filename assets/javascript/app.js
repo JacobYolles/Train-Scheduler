42a297604f3a0dd45a4815ed7e2512855cdc7361
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-08-16 10:03:25 -0600

    Made the project work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,8 +23,8 @@ $("#add-train-btn").on("click", function(event) {
   // CHANGES THE HTML
   var trainName = $("#train-name-input").val().trim();
   var trainDestination = $("#destination-input").val().trim();
-  var trainStart = $("#first-train-input").val().trim(); // CHANGE TO TRAIN FIRST TIME
-  var trainRate = $("#frequency-input").val().trim(); // CHANGE TO TRAIN FREQUENCY
+  var trainStart = $("#first-train-input").val().trim(); 
+  var trainRate = $("#frequency-input").val().trim(); 
   // CREATES THE TEMPORARY VARIABLE FOR HOLDING THE DATA OF THE TRAIN, THIS ALLOWS IT TO BECOME MULTIPLE SUBMISSIONS.
   var newTrain = {
     name: trainName,
@@ -39,27 +39,59 @@ $("#add-train-btn").on("click", function(event) {
   // CLEARS ALL OF THE DATA AFTER THE FORM IS SUBMITTED.
   $("#train-name-input").val(""); // TRAIN NAME
   $("#destination-input").val("");  // TRAIN DESTINATION
-  $("#first-train-input-input").val("");  //FIRST TRAIN TIME
+  $("#first-train-input").val("");  //FIRST TRAIN TIME
   $("#frequency-input").val("");  // TRAIN FREQUENCY
 }); // END OF THE ON CLICK EVENT.
 //
-database.ref().on('child_added', getData, error);
+database.ref().on("child_added", getData, error);
 
 function getData(snapshot) {
-  var data = snapshot.val();
-
-  var startTime = moment(data.start, 'HH:mm');
-  console.log(startTime);
-  var difference = moment().diff(moment(startTime), 'minutes');
+  var data = snapshot.val();  // Data Definition: 
+  console.log(data) // CONSOLE LOG SNAPSHOT. VAL
+  console.log( "      ");
+  var startTime = moment(data.start, "HH:mm");
+  console.log(startTime); // CONSOLE LOG SNAPSHOT.VAL.START DRILL set to HOUR then MINUTE FORMAT.
+  console.log( "      ");
+  var difference = moment().diff(moment(startTime), "minutes");
   console.log(difference);
+  console.log( "      "); // CONSOLE LOG SPACERS
   var remainder = difference % parseInt(data.rate);
   console.log(remainder);
-  var minutesAway = parseInt(data.rate) - remainder;
+  console.log( "      ");
+  var minutesAway = parseInt(data.rate) - remainder; // THIS FIX SHOULDN'T WORK
   console.log(minutesAway);
-  var nextArrival = moment().add(minutesAway, 'minutes');
-  nextArrival = moment(nextArrival).format('HH:mm');
+  console.log( "      ");
+  var nextArrival = moment().add(minutesAway, "minutes");
+  nextArrival = moment(nextArrival).format("HH:mm");
   console.log(nextArrival);
+  console.log( "      ");
 
-} // END OF THE FUNCTION GET DATA(SNAPSHOT) FUNCTION.
+
+  // Table Variables
+  var name = data.name;
+  var dest = data.destination;
+  var start = data.start;
+  var frequency = data.rate;
+
+  var tableRow = $("<tr>");
+
+  var tableData = "<td>" + name + "</td>" +
+                  "<td>" + dest + "</td>" +
+                  "<td>" + start + "</td>" +
+                  "<td>" + frequency + "</td>" +
+                  "<td>" + nextArrival + "</td>" +
+                  "<td>" + minutesAway + "</td>";
+
+  tableRow.prepend(tableData);
+
+  $("#train-table").append(tableRow);
+}  // END OF THE FUNCTION GET DATA(SNAPSHOT) FUNCTION.
+
+function error(error) {  // FUNCTION ERROR START POINT
+  console.log(error);
+} // FUNCTION ERROR END POINT.
+
+
+
 
 }); // DOCUMENT READY FUNCTION END.
